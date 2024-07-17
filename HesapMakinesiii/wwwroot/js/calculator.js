51aa29b2e397e0f117e3e3dd2b3dca57c0e81e7b
ArdaYldrm28 <ardayildiriim8@gmail.com>
2024-07-17 16:28:55 +0300

    AdvancedCalculator

---
+++ HesapMakinesiii/wwwroot/js/calculator.js
@@ -0,0 +1,17 @@
+﻿let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateAndAppend() {
+    try {
+        display.value = eval(display.value) || '';
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
